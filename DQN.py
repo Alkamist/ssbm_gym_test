882757ea15a7979c2a5bf2443681774cc5080f42
Corey Lehman <coreylehman@me.com>
2020-06-01 15:08:09 -0400

    Added some code to be able to test the network while training.

--- DQN.py
+++ DQN.py
@@ -7,51 +7,51 @@ from torch.distributions import Categorical
 import random
 
 
-class ResidualBlock(nn.Module):
-    """ https://arxiv.org/abs/1806.10909 """
-    def __init__(self, input_size, hidden_size):
-        super(ResidualBlock, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.mlp = nn.Sequential(
-            nn.Linear(self.input_size, self.hidden_size),
-            nn.ReLU(True),
-            nn.Linear(self.hidden_size, self.input_size),
-            nn.ReLU(True),
-        )
-
-    def forward(self, x):
-        return x + self.mlp(x)
+#class ResidualBlock(nn.Module):
+#    """ https://arxiv.org/abs/1806.10909 """
+#    def __init__(self, input_size, hidden_size):
+#        super(ResidualBlock, self).__init__()
+#        self.input_size = input_size
+#        self.hidden_size = hidden_size
+#        self.mlp = nn.Sequential(
+#            nn.Linear(self.input_size, self.hidden_size),
+#            nn.ReLU(True),
+#            nn.Linear(self.hidden_size, self.input_size),
+#            nn.ReLU(True),
+#        )
+#
+#    def forward(self, x):
+#        return x + self.mlp(x)
+#
+#
+#class Policy(nn.Module):
+#    def __init__(self, input_size, output_size, hidden_size=64):
+#        super(Policy, self).__init__()
+#        self.res1 = ResidualBlock(input_size, hidden_size)
+#        self.res2 = ResidualBlock(input_size, hidden_size)
+#        self.output = nn.Linear(input_size, output_size)
+#
+#    def forward(self, x):
+#        x = F.relu(self.res1(x))
+#        x = F.relu(self.res2(x))
+#        return self.output(x)
 
 
 class Policy(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size=64):
+    def __init__(self, input_size, output_size, hidden_size=512):
         super(Policy, self).__init__()
-        self.res1 = ResidualBlock(input_size, hidden_size)
-        self.res2 = ResidualBlock(input_size, hidden_size)
-        self.output = nn.Linear(input_size, output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.res1(x))
-        x = F.relu(self.res2(x))
-        return self.output(x)
-
-#
-#class Policy(nn.Module):
-#    def __init__(self, input_size, output_size, hidden_size=512):
-#        super(Policy, self).__init__()
-#        self.fc1 = nn.Linear(input_size, hidden_size)
-#        self.fc2 = nn.Linear(hidden_size, hidden_size)
-#        self.fc3 = nn.Linear(hidden_size, output_size)
-#
-#    def forward(self, x):
-#        x = F.relu(self.fc1(x))
-#        x = F.relu(self.fc2(x))
-#        return self.fc3(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
 
 
 class DQN():
-    def __init__(self, state_size, action_size, device, lr=3e-5, gamma=0.99, target_update_frequency=1000):
+    def __init__(self, state_size, action_size, device, lr=3e-5, gamma=0.99, target_update_frequency=2500):
         self.state_size = state_size
         self.action_size = action_size
         self.lr = lr
@@ -108,7 +108,3 @@ class DQN():
             self.target_net.load_state_dict(self.policy_net.state_dict())
 
         self.learn_iterations += 1
-
-    #def _soft_update(self, tau):
-    #    for target_param, local_param in zip(self.target_net.parameters(), self.policy_net.parameters()):
-    #        target_param.data.copy_(tau * local_param.data + (1 - tau) * target_param.data)
--- train.py
+++ train.py
@@ -36,38 +36,43 @@ epsilon_end = 1.0
 epsilon_decay = 10000
 
 
-#def select_actions(policy, states, epsilon):
-#    with torch.no_grad():
-#        if random.random() > epsilon:
-#            return policy(states).max(1)[1]
-#        else:
-#            return torch.tensor([random.randrange(MeleeEnv.num_actions) for _ in range(2)], device=device, dtype=torch.long)
+def test_policy(policy, testing_steps):
+    env = MeleeEnv(worker_id=1024, **melee_options)
+
+    states = env.reset()
+    states = torch.tensor(states, dtype=torch.float32, device=device)
+
+    total_rewards = 0.0
+
+    for _ in range(testing_steps):
+        try:
+            actions = policy(states).max(1)[1]
+
+            step_env_with_timeout = timeout(5)(lambda : env.step(actions))
+            states, rewards, _, _ = step_env_with_timeout()
+            states = torch.tensor(states, dtype=torch.float32, device=device)
+
+            total_rewards += rewards[0]
+
+        except KeyboardInterrupt:
+            env.close()
+
+        except:
+            env.close()
+            return 0.0
+
+    env.close()
+    return total_rewards / testing_steps
 
 
-#def select_actions(policy, states):
-#    with torch.no_grad():
-#        return torch.multinomial(policy(states), num_samples=1)
-
-
-def generate_frames(worker_id, shared_state_dict, frame_queue, epsilon):
-    #policy = Policy(MeleeEnv.observation_size, MeleeEnv.num_actions).to(device)
-    #policy.load_state_dict(shared_state_dict.state_dict())
-
+def generate_frames(worker_id, frame_queue):
     env = MeleeEnv(worker_id=worker_id, **melee_options)
 
     states = env.reset()
-    #states = torch.tensor(states, dtype=torch.float32, device=device)
 
-    #frame_count = 0
     while True:
         try:
-            #frame_count += 1
-            #if frame_count % 600 == 0:
-            #    policy.load_state_dict(shared_state_dict.state_dict())
-
             actions = [random.randrange(MeleeEnv.num_actions), random.randrange(MeleeEnv.num_actions)]
-            #actions = select_actions(policy, states, epsilon.value)
-            #actions = select_actions(policy, states)
 
             step_env_with_timeout = timeout(5)(lambda : env.step(actions))
             next_states, rewards, _, _ = step_env_with_timeout()
@@ -78,14 +83,7 @@ def generate_frames(worker_id, shared_state_dict, frame_queue, epsilon):
                                 next_states[player_id],
                                 rewards[player_id]))
 
-#            for player_id in range(2):
-#                frame_queue.put((states[player_id].cpu().numpy(),
-#                                 actions[player_id].item(),
-#                                 next_states[player_id],
-#                                 rewards[player_id]))
-
             states = deepcopy(next_states)
-            #states = torch.tensor(states, dtype=torch.float32, device=device)
 
         except KeyboardInterrupt:
             env.close()
@@ -114,24 +112,23 @@ def add_frames_to_replay_buffer(info_flags, replay_buffer, frame_queue):
             info_flags["learns_allowed"] += 1
 
 
+def create_worker(worker_id, frame_queue):
+    p = mp.Process(target=generate_frames, args=(worker_id, frame_queue))
+    p.start()
+    return p
+
+
 if __name__ == "__main__":
     network = DQN(MeleeEnv.observation_size, MeleeEnv.num_actions, device)
     #network.load("checkpoints/agent.pth")
 
-    shared_state_dict = Policy(MeleeEnv.observation_size, MeleeEnv.num_actions)
-    shared_state_dict.load_state_dict(network.policy_net.state_dict())
-    shared_state_dict.share_memory()
-
     replay_buffer = ReplayBuffer(replay_buffer_size, device)
 
     frame_queue = mp.Queue()
-    epsilon = mp.Value("d", epsilon_start)
 
     generator_processes = []
     for worker_id in range(num_workers):
-        p = mp.Process(target=generate_frames, args=(worker_id, shared_state_dict, frame_queue, epsilon))
-        p.start()
-        generator_processes.append(p)
+        generator_processes.append(create_worker(worker_id, frame_queue))
 
     info_flags = {
         "frames_generated" : 0,
@@ -139,7 +136,10 @@ if __name__ == "__main__":
         "dolphin_crashed" : None,
     }
 
-    replay_buffer_frame_thread = threading.Thread(target=add_frames_to_replay_buffer, args=(info_flags, replay_buffer, frame_queue))
+    replay_buffer_frame_thread = threading.Thread(
+        target=add_frames_to_replay_buffer,
+        args=(info_flags, replay_buffer, frame_queue)
+    )
     replay_buffer_frame_thread.start()
 
     need_to_save = False
@@ -148,9 +148,7 @@ if __name__ == "__main__":
         if info_flags["dolphin_crashed"] is not None:
             print("A dolphin instance crashed.")
             generator_processes[info_flags["dolphin_crashed"]].terminate()
-            p = mp.Process(target=generate_frames, args=(info_flags["dolphin_crashed"], shared_state_dict, frame_queue, epsilon))
-            p.start()
-            generator_processes[info_flags["dolphin_crashed"]] = p
+            generator_processes[info_flags["dolphin_crashed"]] = create_worker(info_flags["dolphin_crashed"], frame_queue)
             info_flags["dolphin_crashed"] = None
 
         should_learn = info_flags["learns_allowed"] > 0 and len(replay_buffer) > batch_size
@@ -158,21 +156,19 @@ if __name__ == "__main__":
         if should_learn:
             network.learn(replay_buffer.sample(batch_size))
 
-            epsilon.value = epsilon_end + (epsilon_start - epsilon_end) * math.exp(-1.0 * learn_iterations / epsilon_decay)
-            shared_state_dict.load_state_dict(network.policy_net.state_dict())
-
             learn_iterations += 1
             info_flags["learns_allowed"] -= 1
             need_to_save = True
 
         if learn_iterations % save_every == 0 and learn_iterations > 0 and need_to_save:
-            print("Total Frames: {} / Learn Iterations: {} / Epsilon: {:.3f}".format(
-                info_flags["frames_generated"],
-                learn_iterations,
-                epsilon.value,
-            ))
             network.save("checkpoints/agent" + str(learn_iterations) + ".pth")
             need_to_save = False
+            average_reward = test_policy(network.policy_net, 3600)
+            print("Total Frames: {} / Learn Iterations: {} / Average Reward: {:.3f}".format(
+                info_flags["frames_generated"],
+                learn_iterations,
+                average_reward,
+            ))
 
     replay_buffer_frame_thread.join()
     for p in generator_processes:
